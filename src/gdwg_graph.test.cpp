0d7ce8fcd2453c66404f278b9b2219a41fb76b2f
Yixuan <z5577113@ad.unsw.edu.au>
2024-07-27 21:52:26 +1000

    change section of move test

--- src/gdwg_graph.test.cpp
+++ src/gdwg_graph.test.cpp
@@ -10,20 +10,20 @@ TEST_CASE("gdwg::graph") {
 			auto g2 = gdwg::graph<double, double>{};
 			CHECK(g2.empty());
 		}
-	}
-	SECTION("Move constructor") {
-		auto g1 = gdwg::graph<int, std::string>{1, 2, 3};
-		auto n = 5;
-		g1.insert_node(n);
-		auto g2 = std::move(g1);
-		CHECK(not g1.is_node(1));
-		CHECK(not g1.is_node(2));
-		CHECK(not g1.is_node(3));
-		CHECK(not g1.is_node(5));
-		CHECK(g1.empty());
-		CHECK(g2.is_node(1));
-		CHECK(g2.is_node(2));
-		CHECK(g2.is_node(3));
-		CHECK(g2.is_node(5));
+		SECTION("Move constructor") {
+			auto g1 = gdwg::graph<int, std::string>{1, 2, 3};
+			auto n = 5;
+			g1.insert_node(n);
+			auto g2 = std::move(g1);
+			CHECK(not g1.is_node(1));
+			CHECK(not g1.is_node(2));
+			CHECK(not g1.is_node(3));
+			CHECK(not g1.is_node(5));
+			CHECK(g1.empty());
+			CHECK(g2.is_node(1));
+			CHECK(g2.is_node(2));
+			CHECK(g2.is_node(3));
+			CHECK(g2.is_node(5));
+		}
 	}
 }
