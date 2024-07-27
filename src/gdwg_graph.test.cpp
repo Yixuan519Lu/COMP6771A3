e186bbab868191b54eb4a0e62b88fbba92b94837
Yixuan <z5577113@ad.unsw.edu.au>
2024-07-27 22:00:36 +1000

    add Extractor test

--- src/gdwg_graph.test.cpp
+++ src/gdwg_graph.test.cpp
@@ -26,4 +26,63 @@ TEST_CASE("gdwg::graph") {
 			CHECK(g2.is_node(5));
 		}
 	}
+	SECTION("Extractor") {
+		SECTION("Example Test") {
+			using graph = gdwg::graph<int, int>;
+			auto const v = std::vector<std::tuple<int, int, std::optional<int>>>{
+			    {4, 1, -4},
+			    {3, 2, 2},
+			    {2, 4, std::nullopt},
+			    {2, 1, 1},
+			    {6, 2, 5},
+			    {6, 3, 10},
+			    {1, 5, -1},
+			    {3, 6, -8},
+			    {4, 5, 3},
+			    {5, 2, std::nullopt},
+			};
+
+			auto g = graph{};
+			for (const auto& [from, to, weight] : v) {
+				g.insert_node(from);
+				g.insert_node(to);
+				if (weight.has_value()) {
+					g.insert_edge(from, to, weight.value());
+				}
+				else {
+					g.insert_edge(from, to);
+				}
+			}
+			g.insert_node(64);
+			auto out = std::ostringstream{};
+			out << g;
+			auto const expected_output = std::string_view(R"(
+1 (
+  1 -> 5 | W | -1
+)
+2 (
+  2 -> 4 | U
+  2 -> 1 | W | 1
+)
+3 (
+  3 -> 2 | W | 2
+  3 -> 6 | W | -8
+)
+4 (
+  4 -> 1 | W | -4
+  4 -> 5 | W | 3
+)
+5 (
+  5 -> 2 | U
+)
+6 (
+  6 -> 2 | W | 5
+  6 -> 3 | W | 10
+)
+64 (
+)
+)");
+			CHECK(out.str() == expected_output);
+		}
+	}
 }
