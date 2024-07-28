2b3016517fb025979f92b62560b72503ede9ad29
yixuan lu <z5577113@ad.unsw.edu.au>
2024-07-28 00:34:33 -0700

    fix ==

--- src/gdwg_graph.h
+++ src/gdwg_graph.h
@@ -157,7 +157,7 @@ namespace gdwg {
 				return temp;
 			}
 			auto operator==(my_iterator const& other) const -> bool {
-				return outer_begin == other.outer_begin && inner_ == other.inner_;
+				return outer_begin_ == other.outer_begin_ && inner_ == other.inner_;
 			}
 
 		 private:
